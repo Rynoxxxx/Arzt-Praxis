b1846f4db41251023f4087587dc0a21bdec64de9
samuel.stroot <samuel.stroot@gmail.com>
2023-11-21 13:26:36 +0100

    tdfhd

--- src/Praxis.java
+++ src/Praxis.java
@@ -1,17 +1,54 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Praxis {
 
     Patient erster;
 
-    Praxis() {
+    private JPanel arzt;
+    private JTextField nameText;
+    private JTextField nachnameText;
+    private JButton aufnehmenButton;
+    private JTextField nextPatient;
+    private JButton aufrufenButton;
+    private JLabel name;
+    private JLabel nachname;
+    private JLabel patient2;
+
+    public Praxis(){
+
+        aufnehmenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                aufnehmen();
+
+                nextPatient.setText(nachnameText.getText()+","+" "+nameText.getText());
+
+            }
+        });
+        aufrufenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            aufrufen();
+            nextPatient.setText(erster.getNachfolger().getName()+" "+erster.getNachfolger().getVorname());
+            erster=erster.getNachfolger();
+            }
+
+        });
 
     }
-
-    public void aufnehmen(Patient pPatient) {
+    public void aufnehmen() {
+        Patient pPatient=new Patient(nachnameText.getText(),nameText.getText());
         if (erster == null) {
             erster = pPatient;
+        }else{
+            erster.setNachfolger(pPatient);
         }
-        while (erster != null) {
-            erster.getNachfolger().setNachfolger(pPatient);
+        if (erster != null) {
+            nextPatient.setText(erster.getName()+","+" "+erster.getVorname());
+        }else {
+            nextPatient.setText("");
         }
     }
 
@@ -22,5 +59,17 @@ public class Praxis {
         }
 
         return null;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("arzt");
+        frame.setContentPane(new Praxis().arzt);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
---
+++ src/Wartezimmer.java
@@ -0,0 +1,11 @@
+
+
+
+public class Wartezimmer {
+
+    Patient erster;
+    public Wartezimmer(){
+
+    }
+
+}
